5eb1bc560b0e6be69db8247be8d52522540e0293
AlexanderShaykin <shaykinspicewolf@gmail.com>
2023-02-22 19:15:32 +0300

    hw02 refactor to struct

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -6,30 +6,40 @@ import (
 	"unicode"
 )
 
+type Symbol struct {
+	Value    rune
+	IsLetter bool
+	IsDigit  bool
+	IsSlash  bool
+}
+
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
 	var sb strings.Builder
-	var prev rune = 0
+	var prev Symbol
 	for i, val := range input {
+		current := Symbol{
+			Value: val,
+		}
 		isLast := i == len(input)-1
 
-		if unicode.IsLetter(val) {
-			if prev > 0 {
-				sb.WriteRune(prev)
+		if unicode.IsLetter(current.Value) {
+			if prev.Value > 0 {
+				sb.WriteRune(prev.Value)
 			}
 			if isLast {
-				sb.WriteRune(val)
+				sb.WriteRune(current.Value)
 			}
 
-			prev = val
+			prev = current
 		}
-		if unicode.IsDigit(val) {
-			if prev > 0 {
-				for i := 0; i < int(val-'0'); i++ {
-					sb.WriteRune(prev)
+		if unicode.IsDigit(current.Value) {
+			if prev.Value > 0 {
+				for i := 0; i < int(current.Value-'0'); i++ {
+					sb.WriteRune(prev.Value)
 				}
-				prev = 0
+				prev.Value = 0
 			} else {
 				return "", ErrInvalidString
 			}
