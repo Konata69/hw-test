5a8146504e3848368d29251bd6880ed758734370
AlexanderShaykin <shaykinspicewolf@gmail.com>
2023-02-22 16:07:47 +0300

    hw02 base

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var sb strings.Builder
+	var prev rune = 0
+	for i, val := range input {
+		isLast := i == len(input)-1
+
+		if unicode.IsLetter(val) {
+			if prev > 0 {
+				sb.WriteRune(prev)
+			}
+			if isLast {
+				sb.WriteRune(val)
+			}
+
+			prev = val
+		}
+		if unicode.IsDigit(val) {
+			if prev > 0 {
+				for i := 0; i < int(val-'0'); i++ {
+					sb.WriteRune(prev)
+				}
+				prev = 0
+			} else {
+				return "", ErrInvalidString
+			}
+		}
+	}
+
+	return sb.String(), nil
 }
