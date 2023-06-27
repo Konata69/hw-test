f24b070ddccfd0b1f28fda1e3b7589a9c0ff28e1
AlexanderShaykin <shaykinspicewolf@gmail.com>
2023-06-27 17:11:53 +0300

    hw08 env_reader

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -12,5 +19,38 @@ type EnvValue struct {
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
 	// Place your code here
-	return nil, nil
+	dirEntry, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	envMap := make(Environment)
+	for _, entry := range dirEntry {
+		info, _ := entry.Info()
+
+		path := dir + "/" + info.Name()
+		file, err := os.Open(path)
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		scanner.Scan()
+		valueString := scanner.Text()
+		valueString = string(bytes.TrimRight(bytes.ReplaceAll([]byte(valueString), []byte("\x00"), []byte("\n")), " "))
+
+		needRemove := info.Size() == 0
+
+		name := info.Name()
+		name = strings.ReplaceAll(name, "=", "")
+		value := EnvValue{
+			Value:      valueString,
+			NeedRemove: needRemove,
+		}
+
+		envMap[name] = value
+	}
+
+	return envMap, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,74 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+const tmpFolder = "./tmp"
+
+func checkErr(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func makeFile(name string, body string) {
+	file, err := os.Create(tmpFolder + "/" + name)
+	checkErr(err)
+	defer file.Close()
+
+	_, err = file.WriteString(body)
+	checkErr(err)
+}
+
+func setUp() {
+	err := os.Mkdir(tmpFolder, os.ModePerm)
+	checkErr(err)
+
+	makeFile("BAR", "bar")
+	makeFile("EMPTY", "")
+	makeFile("SPACE", "space  ")
+	makeFile("NULL", "null\x00")
+	makeFile("EQUAL=", "equal")
+}
+
+func tearDown() {
+	err := os.RemoveAll(tmpFolder)
+	checkErr(err)
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	expected := Environment{
+		"BAR": {
+			Value:      "bar",
+			NeedRemove: false,
+		},
+		"EMPTY": {
+			Value:      "",
+			NeedRemove: true,
+		},
+		"SPACE": {
+			Value:      "space",
+			NeedRemove: false,
+		},
+		"NULL": {
+			Value:      "null\n",
+			NeedRemove: false,
+		},
+		"EQUAL": {
+			Value:      "equal",
+			NeedRemove: false,
+		},
+	}
+
+	setUp()
+	defer tearDown()
+	result, err := ReadDir(tmpFolder)
+	checkErr(err)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("result:\n %v\n\n expected:\n %v", result, expected)
+	}
 }
