7977cd8051970f54def575b807bdd57d0fb816df
AlexanderShaykin <shaykinspicewolf@gmail.com>
2023-06-27 18:44:32 +0300

    hw08 executor

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	expectedEnvName := "BAR"
+	expectedEnvValue := "bar"
+	expectedReturnCode := 0
+
+	returnCode := RunCmd(
+		[]string{"echo"},
+		map[string]EnvValue{
+			"BAR": {
+				Value:      "bar",
+				NeedRemove: false,
+			},
+		})
+
+	os.LookupEnv(expectedEnvName)
+	require.Equal(t, os.Getenv(expectedEnvName), expectedEnvValue)
+
+	if returnCode != expectedReturnCode {
+		t.Errorf("ReturnCode\n result: %v\n expected: %v", returnCode, expectedReturnCode)
+	}
 }
